683e580c24aad0de26ca05eeaf0590feae08b0d5
VagabondPeak <peter.ptai.nguyen@gmail.com>
2025-07-27 02:43:26 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,1 +1,15 @@
+//libaries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "shell.h"
 
+//function to prompt the user
+void printPrompt()
+{
+  printf("osshell$ ");
+  fflush(stdout);
+}
