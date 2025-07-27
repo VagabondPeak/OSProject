73aedfe66c2c2c49129aab4b2c1a8e2e09d86174
VagabondPeak <peter.ptai.nguyen@gmail.com>
2025-07-27 14:17:03 -0500

    Fix header file
    Fixed some issues, changed struct to typedef struct, added a semi colon, added new constant variable MAX_CMDLINE

--- shell.h
+++ shell.h
@@ -1,23 +1,30 @@
 //head file to define some variables constants, a struct for command
 
+#ifndef SHELL_H
+#define SHELL_H
 
 #define LINE 80 //max line length
 #define MAX_ARGS 64 //max num of args
 #define MAX_ARG_LEN 16 //max arg length
 #define MAX_PATHS 64 //max num of paths
 #define MAX_PATH_LEN 96 //max path length
+#define MAX_CMDLINE 1024 //input buffer size
 #define WHITESPACE " \t\n" //whitespace separators
 #define TRUE 1 //make a true value 1
 
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
-  
-struct command_t {
+
+//typedef struct to group cmd variables together
+typedef struct {
   char *name;
   int argc;
   char *argv[MAX_ARGS];
-}
+} command_t;
+
+
+//prototypes for commands
 void readCommand(char* inputBuffer);
 void parseCommand(char* inputBuffer, command_t* cmd);
 int parsePath(char* dir[]);
